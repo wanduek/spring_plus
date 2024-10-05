2294877540160494de52eda31a2b43c32b862f6a
wanduek <minjoois2@gmail.com>
2024-10-05 21:23:40 +0900

    level2:9.Spring Security

--- src/main/java/org/example/expert/domain/user/entity/User.java
+++ src/main/java/org/example/expert/domain/user/entity/User.java
@@ -1,15 +1,22 @@
 package org.example.expert.domain.user.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.common.entity.Timestamped;
 import org.example.expert.domain.user.enums.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
 
 @Getter
 @Entity
+@AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Table(name = "users")
 public class User extends Timestamped {
 
@@ -37,10 +44,19 @@ public class User extends Timestamped {
         this.id = id;
         this.email = email;
         this.userRole = userRole;
+
     }
 
     public static User fromAuthUser(AuthUser authUser) {
-        return new User(authUser.getId(), authUser.getEmail(), authUser.getUserRole());
+        return  User.builder()
+                .id(authUser.getId())
+                .email(authUser.getEmail())
+                .userRole(UserRole.of(authUser.getAuthorities().stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .findFirst() // 첫 번째 권한을 가져옴
+                        .orElse(UserRole.ROLE_USER.name())) // 기본 ROLE 설정
+                )
+                .build();
     }
 
     public void changePassword(String password) {
